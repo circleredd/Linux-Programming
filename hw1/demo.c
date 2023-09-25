eb3fdf00c7d964a84162d2f917db4c4d65b228ae
circleredd <alanfang229@gmail.com>
2023-09-25 21:36:09 +0800

    hw2-tail

--- hw1/demo.c
+++ hw1/demo.c
@@ -3,23 +3,26 @@
 #include <unistd.h>
 #include "errExit.h"
 
-int
-main(int argc, char *argv[]){
-    int fd;
-    ssize_t numWritten;
-    char *buf = &argv[2][0];
-    fd = open(argv[1], O_RDWR);
-    if(fd == -1)    
-        // errexit("open");
-        return -1;
-    numWritten = write(fd, buf, 4);
-    if(numWritten == -1)
-	    return -1;
-    if(close(fd) == -1)
-        // errexit("close");
-        return -1;
-    printf("Success\n");
-    printf("%s %s\n", argv[0], argv[1]);
-    return 0;
-    
+int main(int argc, char *argv[])
+{
+    // int fd;
+    // ssize_t numWritten;
+    // char *buf = &argv[2][0];
+    // fd = open(argv[1], O_RDWR);
+    // if(fd == -1)
+    //     // errexit("open");
+    //     return -1;
+    // numWritten = write(fd, buf, 4);
+    // if(numWritten == -1)
+    //     return -1;
+    // if(close(fd) == -1)
+    //     // errexit("close");
+    //     return -1;
+    // printf("Success\n");
+    // printf("%s %s\n", argv[0], argv[1]);
+    // return 0;
+    char buf[BUFSIZ];
+    read(STDIN_FILENO, buf, sizeof(buf));
+    printf("%s\n", buf);
+    printf("argc:%d, argv[0]:%s, argv[1]:%s, argv[2]:%s, argv[3]:%s\n", argc, argv[0], argv[1], argv[2], argv[3]);
 }
---
+++ hw2/1/tail.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "errExit.h"
+/*
+./tail filename
+./tail -n num filename
+*/
+
+int main(int argc, char **argv)
+{
+	int opt, numLines = 10; // opt and number of tail lines we want to fetch, by default is 10
+	int fd;					// the opened file flag
+	int fileArgvIndex = 1;	// the argv index of the inputfile, by default is 1
+	long int numRead;		// number of char read from the file
+	char buf[1024];
+
+	if (argc != 2 && argc != 4)
+		errexit("Usage: %s filename or %s -n num filename", argv[0], argv[0]);
+
+	while ((opt = getopt(argc, argv, "n:")) != -1)
+	{
+		switch (opt)
+		{
+		case 'n':
+			numLines = atoi(optarg); // convert the optarg from string to int
+			fileArgvIndex = 3;
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	if ((fd = open(argv[fileArgvIndex], O_RDONLY)) == -1)
+		errexit("Fail to open");
+
+	if ((numRead = read(fd, buf, 1024)) == -1)
+		errexit("Fail to read!");
+	buf[numRead] = '\0';
+
+	// To find the position we want to write to STDOUT by finding the number of '\n'
+	long int lines = 0;
+	long int cur = -1;
+	for (int i = numRead - 1; i >= 0; i--)
+	{
+		if (buf[i] == '\n')
+			lines++;
+		else if (i > 0 && lines == numLines - 1 && buf[i - 1] == '\n')
+		{
+			cur = i;
+			break;
+		}
+	}
+	cur = (cur == -1) ? 0 : cur;
+
+	if (write(STDOUT_FILENO, &buf[cur], numRead - cur) == -1)
+		errexit("Fail to write!");
+
+	if (close(fd) == -1)
+		errexit("Fail to close the file!");
+
+	return 0;
+}
